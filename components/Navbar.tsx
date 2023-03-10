719d3718819e4c2728e731f5e3c8242b6500cfd1
Yudistira Maulana <yudistir4maulana@gmail.com>
2023-03-10 20:13:06 +0700

    fix syntak navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,68 +1,95 @@
-import { BellIcon } from '@heroicons/react/24/solid'
+import { BellIcon } from '@heroicons/react/24/solid';
 
-import Image from 'next/image'
-import React, { useEffect, useState } from 'react'
-import { FaSearch } from 'react-icons/fa'
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
 
 import {
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
-    MenuItemOption,
-    MenuGroup,
-    MenuOptionGroup,
-    MenuDivider,
-} from '@chakra-ui/react'
-import useAuth from '../hooks/useAuth'
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+} from '@chakra-ui/react';
+import useAuth from '../hooks/useAuth';
+import Link from 'next/link';
 
 const Navbar = () => {
+  const { logout } = useAuth();
+  const [isScrolled, setIsScrolled] = useState(false);
 
-    const { logout } = useAuth()
-    const [isScrolled, setIsScrolled] = useState(false)
+  useEffect(() => {
+    const handleScroll = () => {
+      window.scrollY > 0 ? setIsScrolled(true) : setIsScrolled(false);
+    };
 
-    useEffect(() => {
-        const handleScroll = () => {
-            window.scrollY > 0 ? setIsScrolled(true) : setIsScrolled(false)
-        }
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
-        window.addEventListener('scroll', handleScroll)
-        return () => {
-            window.removeEventListener('scroll', handleScroll)
+  console.log(isScrolled);
+  return (
+    <div
+      className={`${
+        isScrolled && 'bg-[#141414]'
+      } transition duration-300 flex items-center justify-between px-5 py-3 sticky top-0 z-50`}
+    >
+      <div className="flex items-center">
+        <Link href="/">
+          <a>
+            <Image
+              className="cursor-pointer object-contain"
+              alt=""
+              src="/assets/netflix-logo.svg"
+              width={100}
+              height={40}
+            />
+          </a>
+        </Link>
+        <ul className=" items-center gap-3 ml-8 text-[#e5e5e5] hidden md:flex">
+          <li className="hover:text-[#b3b3b3] transition cursor-pointer">
+            Home
+          </li>
+          <li className="hover:text-[#b3b3b3] transition cursor-pointer">
+            Tv Shows
+          </li>
+          <li className="hover:text-[#b3b3b3] transition cursor-pointer">
+            Movies
+          </li>
+          <li className="hover:text-[#b3b3b3] transition cursor-pointer">
+            News & Popular
+          </li>
+          <li className="hover:text-[#b3b3b3] transition cursor-pointer">
+            My List
+          </li>
+        </ul>
+      </div>
 
-        }
-    }, [])
+      <div className="flex items-center gap-3">
+        <FaSearch />
+        <BellIcon className="h-6 w-6 " />
+        <Menu>
+          <MenuButton>
+            <img src="/assets/person.png" className="rounded" alt="" />
+          </MenuButton>
+          <MenuList className="!bg-black !border-none">
+            <MenuItem
+              onClick={logout}
+              _hover={{ backgroundColor: '#333' }}
+              _focus={{ backgroundColor: 'none' }}
+            >
+              Logout
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </div>
+    </div>
+  );
+};
 
-    console.log(isScrolled)
-    return (
-        <div className={`${isScrolled && 'bg-[#141414]'} transition duration-300 flex items-center justify-between p-3 sticky top-0 z-50`}>
-            <div className="flex items-center">
-
-                <Image className="cursor-pointer object-contain" alt='' src="/assets/netflix-logo.svg" width={100} height={40} />
-                <ul className=" items-center gap-3 ml-8 text-[#e5e5e5] hidden md:flex">
-                    <li className="hover:text-[#b3b3b3] transition cursor-pointer">Home</li>
-                    <li className="hover:text-[#b3b3b3] transition cursor-pointer">Tv Shows</li>
-                    <li className="hover:text-[#b3b3b3] transition cursor-pointer">Movies</li>
-                    <li className="hover:text-[#b3b3b3] transition cursor-pointer">News & Popular</li>
-                    <li className="hover:text-[#b3b3b3] transition cursor-pointer">My List</li>
-                </ul>
-            </div>
-
-            <div className="flex items-center gap-3">
-                <FaSearch />
-                <BellIcon className='h-6 w-6 ' />
-                <Menu >
-                    <MenuButton>
-                        <img src="/assets/person.png" className="rounded" alt="" />
-                    </MenuButton>
-                    <MenuList className="!bg-black !border-none">
-                        <MenuItem onClick={logout} _hover={{ backgroundColor: "#333" }} _focus={{ backgroundColor: "none" }}>Logout</MenuItem>
-
-                    </MenuList>
-                </Menu>
-            </div>
-        </div>
-    )
-}
-
-export default Navbar
+export default Navbar;
